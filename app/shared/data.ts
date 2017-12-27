cd6d1fadf8853f4ab50fd3e003382f5b56d44b8a
Anatoliy Simonov <a.simonov@andersenlab.com>
2017-12-27 17:06:44 +0300

    Initial commit

---
+++ app/shared/data.ts
@@ -0,0 +1,10 @@
+export const todos = [{
+    title: 'Learn JS',
+    completed: true
+}, {
+    title: 'Learn Angular 2',
+    completed: false
+}, {
+    title: 'Write application',
+    completed: false
+}];
